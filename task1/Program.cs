bbd98d7173cf520e8931a50a0016098dcba7e703
alexidi <alex.vyrlan@gmail.com>
2024-02-21 14:24:27 +0200

    Add metod print

--- task1/Program.cs
+++ task1/Program.cs
@@ -5,8 +5,13 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 == number2 * number2)
+// if (number1 == number2 * number2)
+// {
+//     Console.WriteLine("Да");
+// }
+// else Console.WriteLine("Нет");
+if (number1 == Math.Pow(number2, 2))
 {
-    Console.WriteLine("Да");
+    Console.WriteLine($"Квадрат числа {number2} равен {number1}");
 }
-else Console.WriteLine("Нет");
+else Console.WriteLine($"Квадрат числа {number2} НЕ равен {number1}");
