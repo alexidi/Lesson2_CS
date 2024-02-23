ea4b3323efc78dddc3e61806543f14476996ec73
alexidi <alex.vyrlan@gmail.com>
2024-02-23 23:46:47 +0200

    Add new program

---
+++ task7/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью с
+// конца цифру заданного числа или сообщает, что
+// третьей цифры нет.
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num < 100)
+{
+    Console.WriteLine("Третей цифры нет!");
+}
+else
+{
+    while (num > 999)
+    {
+        num /= 100;
+    }
+    Console.WriteLine(num % 10);
+}
